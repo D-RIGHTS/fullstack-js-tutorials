05e57da3598ca9e148fb42488deeb65f32872c0b
D-RIGHTS <rodrick.d@hypertext.com.kw>
2021-04-27 12:49:57 +0300

    introduced variables

---
+++ my-javascript/js/var.js
@@ -0,0 +1,33 @@
+
+// This is called declaring a variable
+var message;
+// Assigning a variable
+message = "Welcome to the Variable introduction"
+
+console.log(message);
+
+
+// Change the value in variable
+
+var score = 3;
+// score = score + 10
+
+// this is called an addition assignment operator
+score += 10;
+console.log(score);
+
+
+var bonusPts = 50;
+var finalScore = score + bonusPts
+console.log(finalScore);
+
+
+// let const 
+let mySurname;
+
+mySurname = "Ndhlovu ";
+mySurname += "Dumi "
+mySurname += "is around today "
+
+console.log(mySurname);
+
